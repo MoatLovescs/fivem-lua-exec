72cbc19997b737fff013145e6375cd4d371181a6
MoatLovescs <100001861+MoatLovescs@users.noreply.github.com>
2022-06-04 11:52:03 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include "Paster.h" // we include stuff
+Paster pasting; // we do stuff
+int paster()
+{
+  pasting.PastedLuaExecutor(NULL, 0, 0, 0, 0, 0,0 ,0,0,0,f0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,02,02,2,,2,2,2,2,2,,2); // here we just uh so stuff
+}
