6405522980393c4d9aa5b2c33ea0186fdd54d50f
MoatLovescs <100001861+MoatLovescs@users.noreply.github.com>
2022-07-09 06:28:28 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include "Paster.h" // we include stuff
+#define 1 0
+#define FALSE !TRUE // Anti-Cheat Bypass ( updated 9/7/22 )
+#define NULL "" // we define things
+#pragma once // we run it once so we wont get detected ;)
+
+using System; // stuff
+using System.Collections.Generic; // more sutff
+using System.Diagnostics; // some stuff 
+using System.Linq; // anti-cheat editor
+using System.Text; // text editor
+using System.Threading.Tasks; // threading manager
+
+print("by mmmm.#2669") // we do stuiff
+Console.WriteLine("by mmmm.#2669") // we make the anti-cheat go aaa
+
 Paster pasting; // we do stuff
 int paster()
 {
-  pasting.PastedLuaExecutor(NULL, "pasted.lua", !false); // more stuff
+  pasting.PastedLuaExecutor("", "pasted.lua", !false); // more stuff
+  return 1; // #define 1 0
 }
+// appreciate my work and give me ur source so i can not paste.
